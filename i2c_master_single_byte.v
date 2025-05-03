912ba070402d0eae86c2259ac20f173bb535be0f
FLinLan <maxlan0987@gmail.com>
2025-05-03 10:11:01 -0700

    added module instantiation for controller the file for master interface sending single byte

---
+++ i2c_master_single_byte.v
@@ -0,0 +1,72 @@
+//`include "timescale.v"
+//// synopsys translate_on
+//
+//`include "i2c_master_defines.v"
+
+module i2c_master_single_byte #(parameter CLK_RATIO = 25)
+	(input i_Clk,					  // Main FPGA clock
+	 input i_Rst,					  // Active-high asychronous reset
+	 input i_Enable,				  // Enable for I2C master
+	 //
+	 input [6:0] i_Slave_Addr,	  // I2C Address of Slave device
+	 input i_Wr_Start,			  // Kicks off a write when pulsed
+	 input i_Rd_Start,			  // Kicks off a read when pulsed
+	 input [7:0] i_Wr_Byte,		  // Data to write (if write cmd)
+	 output reg o_Busy,			  // Falling edge = done
+	 output [7:0] o_Rd_Byte,	  // Result of a read
+	 output o_Error,				  // High if error in lost transcation
+	 //
+	 input io_scl,					  // Actual I2C Clock
+	 input io_sda					  // Actual I2C Data
+	);
+	
+	// Defines the pper limit of an internal counter. This sets the frequency of the I2C
+	// clock, which depends on teh input clock frequency. For details on this math, see the
+	// I2C User Guide, Section 3.2.1 - Prescale Register
+	localparam [15:0] CLK_COUNT = CLK_RATIO / 5 - 1;
+	
+	wire w_sck_en, w_sda_en;
+	wire w_Arb_Lost, w_Cmd_Ack, w_Slave_Ack;
+	wire w_Cmd_Start;
+	
+	// right now o_done doesn't go to the high level module, is it needed?
+	assign o_Done = w_Arb_Lost | w_Cmd_Ack;
+	
+	// Start can be either read or write, ensure not both. (XOR)
+	assign w_Cmd_Start = i_Wr_Start ^ i_Rd_Start;
+	
+	// i2c master block instantiation
+	// hookup byte controller block
+	i2c_master_byte_ctrl byte_controller (
+		.clk(i_clk),
+		.rst(1'b0), // use async reset below only
+		.nReset(~i_Rst), // active low, invert required
+		.ena(i_Enable), 
+		.clk_cnt(CLK_COUNT),	
+		.start(w_Cmd_Start), // Single clock cycle pulse to start command 
+		.stop(r_Cmd_Stop), // Used to set end of command
+		.read(i_Rd_Start), // Read command
+		.write(i_Wr_Start), // Write command
+		.ack_in(1'b0), // When Master is Receiving, 0=ACK, 1 =NACK, nac can indicate end
+		.din(i_Wr_Byte), 
+		.cmd_ack(w_Cmd_Ack), // From Slave. When Command is done this is set. 
+		.ack_out(W_Slave_Ack), // When Slave is Receiving. 0=ACK, 1=NACK. How to handle situation
+		.dout(o_Rd_Byte),
+		.i2c_busy(), // This module generates its own busy signal
+		.i2c_al(w_Arb_Lost),
+		.scl_i(io_scl),
+		.scl_o(), // tied to gnd inside bit_ctrl, don't need
+		.scl_oen(w_sck_en),
+		.sda_i(io_sda),
+		.sda_o(), // tied to gnd insdie bit_ctrl, don't need
+		.sda_oen(w_sda_en)
+	);
+	 
+	// Creates tri-state buffer.
+	// When Enable is high, go high impedance (1). When low, pull low to zero.
+	assign io_scl = w_sck_en ? 1'bz : 1'b0;
+	assign io_sda = w_sda_en ? 1'bz : 1'b0;
+	
+	
+	
+endmodule
